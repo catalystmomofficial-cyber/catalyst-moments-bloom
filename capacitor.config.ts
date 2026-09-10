d92a7bb95eed9ba9ad54461fc2b2b9012304838a
Cam Bautista <cambautista@Cams-Laptop.local>
2026-09-10 18:31:17 +0800

    Add Capacitor iOS application shell

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.catalystmomofficial.app',
+  appName: 'Catalyst Mom',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
